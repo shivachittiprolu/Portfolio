f2c36a87a1db9e373b751059de49a2da895f3764
shiva chittiprolu <shivachittiprolu@gmail.com>
2021-04-29 13:22:37 +0530

    added animal class to understand inheritance

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,3 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+class Animal {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
+
+  speak (){
+    document.write("HI, I am ",this.name, " and I'm ", this.age, " years old! <br><br>");
+  }
+}
+
+const animal1 = new Animal('Simba',3);
+animal1.speak();
+console.log(animal1);
+
+const animal2 = new Animal('Mufasa', 22);
+animal2.speak();
