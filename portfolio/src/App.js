2e64e1e633eb45aaa877b2a8a286cf7b9c1f05ab
shiva chittiprolu <shivachittiprolu@gmail.com>
2021-04-29 15:52:28 +0530

    Learn setState -- ReactJS -- toggledisplayBio func

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
-class App extends Component{
-    render (){
-        return(
+class App extends Component {
+    constructor() {
+        super();
+        this.state = { displayBio: false };
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    }
+    toggleDisplayBio(){
+        this.setState({displayBio: !this.state.displayBio});
+        
+    }
+    render() {
+        return (
             <div>
                 <h1>Welcome to React!!</h1>
                 <p>I'm Shiva, Software Developer!!</p>
+                <p>Is it working fine ? </p>
+                {this.state.displayBio ? (
+                    <div><p> I love writing code and reading books</p>
+                    <p>besides that , I love listening music too.</p>
+                    <button onClick = {this.toggleDisplayBio}>Read less </button>
+                    </div>
+                    
+                ): (
+                    <div><button onClick = {this.toggleDisplayBio}>Read More </button></div>
+                )
+                }
             </div>
         )
     }
