539e46b42692ad7ffeef9064828e2ae4f400a6fb
shiva chittiprolu <shivachittiprolu@gmail.com>
2021-10-08 14:53:36 +0530

    Project.js

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -1,34 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import PROJECTS from './data/Projects';
 
-class Project extends Component{
-    render () {
-        console.log(this.props.project);
-        const {title, image, description, link } = this.props.project;
-        return (
-            <div style={{display: "inline-block", width: 300, margin: 10}}>
-                <h3>{title}</h3>
-                <img src ={image} alt ='profile' style ={{width: 200, height : 120}}/>
-                <p>{description}</p>
-                <a href ={link}>{link}</a>
-            </div>
-        )
-    }
+const Project = props => {
+    const { title, image, description, link } = props.project;
+    return (
+        <div style={{ display: "inline-block", width: 300, margin: 10 }}>
+            <h3>{title}</h3>
+            <img src={image} alt='profile' style={{ width: 200, height: 120 }} />
+            <p>{description}</p>
+            <a href={link}>{link}</a>
+        </div>
+    )
 }
-class Projects extends Component {
-    render() {
-        return (
+
+const Projects = props => {
+    return (
+        <div>
+            <h2>Highlighted Projects</h2>
             <div>
-                <h2>Highlighted Projects</h2>
-                <div>
-                    {PROJECTS.map(PROJECT => {
-                        return  (
-                            <Project key ={PROJECT.id} project= {PROJECT}></Project>
-                        )
-                    })}
-                </div>
+                {PROJECTS.map(PROJECT => {
+                    return (
+                        <Project key={PROJECT.id} project={PROJECT}></Project>
+                    )
+                })}
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 export default Projects;
