72adc2c2eaf0d17c872013b0426e22fa636fdd23
shiva chittiprolu <shivachittiprolu@gmail.com>
2021-10-08 14:53:14 +0530

    App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
+import ProfileImage from '../src/assets/profile.png';
+import Title from './Title';
+// import Jokes from './Jokes';
 
 class App extends Component {
     constructor() {
@@ -8,32 +11,36 @@ class App extends Component {
         this.state = { displayBio: false };
         this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
     }
-    toggleDisplayBio(){
-        this.setState({displayBio: !this.state.displayBio});
-        
+    toggleDisplayBio() {
+        this.setState({ displayBio: !this.state.displayBio });
+
     }
     render() {
         return (
             <div>
+                <img src={ProfileImage} alt='Profile' className='profile'/>
                 <h1>Hello!!</h1>
-                <p>I'm <b>Shiva</b>, Software Developer!!</p>
-                <p>I'm currently working in <b>Tata Co0nsultancy Services</b> as  <b>Systems Engineer</b>.</p>
+                <p>I'm Shiva, Software Developer.</p>
+                < Title />
+                <p>I'm currently working in Tata Consultancy Services as  Systems Engineer.</p>
                 {this.state.displayBio ? (
                     <div><p> I love writing code and reading books</p>
-                    <p>besides that , I love listening music too.</p>
-                    <button onClick = {this.toggleDisplayBio}>Read less </button>
+                        <p>besides that , I love listening music too.</p>
+                        <button onClick={this.toggleDisplayBio}>Read less </button>
                     </div>
-                    
-                ): (
-                    <div><button onClick = {this.toggleDisplayBio}>Read More </button></div>
+
+                ) : (
+                    <div><button onClick={this.toggleDisplayBio}>Read More </button></div>
                 )
                 }
                 <hr />
-            <Projects />
-            <hr />
-            <SocialProfiles />
+                <Projects />
+                <hr />
+                <SocialProfiles />
+                <hr />
+                {/* <Jokes /> */}
             </div>
-            
+
         )
     }
 }
